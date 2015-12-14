2abc98893972a5d1d67bb075bbe1195acbe06fa8
shanewow <shanewow@users.noreply.github.com>
2015-12-13 22:19:50 -0500

    disabled csrf for now as this version of jhipster seems to be buggy

--- src/main/java/tech/yournew/config/SecurityConfiguration.java
+++ src/main/java/tech/yournew/config/SecurityConfiguration.java
@@ -75,37 +75,18 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .antMatchers("/test/**");
     }
     
-    private RequestMatcher getCSRFRequestMatcher(){
-    	return new RequestMatcher() {
-            private Pattern allowedMethods = Pattern.compile("^(GET|HEAD|TRACE|OPTIONS)$");
-            private RegexRequestMatcher apiMatcher = new RegexRequestMatcher("^(/api/authentication|/api/logout)", null);
-
-            @Override
-            public boolean matches(HttpServletRequest request) {
-                // No CSRF due to allowedMethod
-                if(allowedMethods.matcher(request.getMethod()).matches())
-                    return false;
-
-                // No CSRF due to api call
-                if(apiMatcher.matches(request))
-                    return false;
-
-                // CSRF for everything else that is not an API call or an allowedMethod
-                return true;
-            }
-        };
-    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+    	http.csrf().disable();
         http
-            .csrf()
-            .ignoringAntMatchers("/websocket/**")
-            .ignoringAntMatchers("/api/logout")
-            .ignoringAntMatchers("/api/authentication")
+            //.csrf().
+//            .ignoringAntMatchers("/websocket/**")
+//            .ignoringAntMatchers("/api/logout")
+//            .ignoringAntMatchers("/api/authentication")
             //.requireCsrfProtectionMatcher(getCSRFRequestMatcher())
-        .and()
-            .addFilterAfter(new CsrfCookieGeneratorFilter(), CsrfFilter.class)
+        //.and()
+            //.addFilterAfter(new CsrfCookieGeneratorFilter(), CsrfFilter.class)
             .exceptionHandling()
             .authenticationEntryPoint(authenticationEntryPoint)
         .and()
